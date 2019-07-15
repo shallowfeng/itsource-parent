44e4d663e1f1d6ccb9c726185d74234603a0c26f
shallowfeng <1690554421@qq.com>
2019-07-15 17:43:13 +0800

    单元测试脚本

---
+++ test.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+
+
+class sum(unittest.TestCase):
+    def testone(self):
+        a = 3
+        b = 5
+        self.assertEqual(a,b)
+        print("1")
+
+
+
+    def testtwo(self):
+        a = "we"
+        b = "we"
+        self.assertNotEqual(a,b)
+        print("2")
+
+
+
+    def testthee(self):
+        a = "dss"
+        b = 5
+        self.assertNotEqual(a,b)
+        print("3")
+
+
+
+    def testfour(self):
+        a = "sadfsdf"
+        b = "dsfsf"
+        self.assertNotEqual(a,b)
+        print("4")
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
